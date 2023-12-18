7807f310cb402adc7fd9fdf66d290b56ae41c414
IRISH BANGA <122631145+IrishBanga@users.noreply.github.com>
2023-12-17 22:51:20 -0500

    lab01

--- src/unl/soc/Hello.java
+++ src/unl/soc/Hello.java
@@ -1,17 +1,18 @@
 package unl.soc;
 
 /**
- * Author: Chris Bourke (cbourke@cse.unl.edu) 
- * Date: xx/xx/20xx
+ * Author: Irish Banga
+ * Date: 2023/12/17
  * 
  * A simple "Hello World!" program in Java
  * 
- * @author cbourke
+ * @author Irish Banga
  *
  */
 public class Hello {
 
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
+		System.out.println("This is me learning Java!");
 	}
 }
